efcd30fa5272f8695e5bc1555d71a1dddd8cfd67
penetrick0 <penetrick0@gmail.com>
2022-03-13 06:25:06 -0700

    commit

---
+++ cpp/1037.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+void sort(int *ptr, int count)
+{
+    int tmp = 0;
+
+    for(int i = count - 1; i > 0; i--)
+    {
+        for(int j = 0; j < i; j++)
+        {
+            if(*(ptr + j) > *(ptr + i))
+            {
+                tmp = *(ptr + j);
+                *(ptr + j) = *(ptr + i);
+                *(ptr + i) = tmp;
+            }
+        }
+    }
+}
+
+int main(void)
+{
+    int count;
+    int *arr;
+
+    cin >> count;
+    arr = new int[count];
+
+    for(int i = 0; i < count; i++)
+        cin >> *(arr + i);
+    
+    sort(arr, count);
+
+    cout << *(arr + 0) * *(arr + (count - 1)) << endl;
+
+    delete[] arr;
+
+    return 0;
+}
